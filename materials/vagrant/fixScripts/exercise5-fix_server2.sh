a181df74cec942efa123e6e797b4dbe16743dc11
Yogev benishu <yogevqwe1@gmail.com>
2024-10-05 19:58:51 +0300

    Yogev solution 100% Fix

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -5,6 +5,7 @@ SERVER1_IP="192.168.60.10"
 SERVER2_IP="192.168.60.11"
 SERVER_USER="vagrant"
 PASSWORD="vagrant"
+SSH_CONFIG_FILE="/home/vagrant/.ssh/config"
 
 sudo apt-get update && sudo apt-get install -y sshpass
 
@@ -22,3 +23,10 @@ sshpass -p "$PASSWORD" ssh "$SERVER_USER@$SERVER1_IP" \
 ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa
 
 sshpass -p "$PASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no "$SERVER_USER@$SERVER1_IP"
+
+
+sudo -u vagrant touch $SSH_CONFIG_FILE
+echo "Host $SERVER1_IP" >> $SSH_CONFIG_FILE
+echo "    StrictHostKeyChecking no" >> $SSH_CONFIG_FILE
+echo "    UserKnownHostsFile=/dev/null" >> $SSH_CONFIG_FILE
+
